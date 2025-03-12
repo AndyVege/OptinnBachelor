3450aa68d29090aaeb36bbccb9695e3eecc7571e
mustaf630 <75674707+mustaf630@users.noreply.github.com>
2025-03-12 13:21:11 +0100

    set up neon

--- optinnbachelor/db/index.ts
+++ optinnbachelor/db/index.ts
@@ -1,3 +1,5 @@
-import 'dotenv/config';
-import { drizzle } from 'drizzle-orm/node-postgres';
-export const db = drizzle(process.env.DATABASE_URL!);
+import { neon } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
+
+const sql = neon(process.env.DATABASE_URL!);
+export const db = drizzle({ client: sql });
