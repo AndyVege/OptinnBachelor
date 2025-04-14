f56bfbba1f69e12ac2855a79e2af93f6d962cff4
barslang <lursbang@gmail.com>
2025-04-14 13:34:19 +0200

    Riktig API spørringer

--- optinnbachelor/app/api/fetch_data/fetch_HelseData.ts
+++ optinnbachelor/app/api/fetch_data/fetch_HelseData.ts
@@ -1,20 +1,46 @@
 export async function fetch_HelseData() {
-    const url_Helse = "https://data.ssb.no/api/v0/no/table/12448/";
-  
-    const payload = {
-      "query": [
-        { "code": "Region", "selection": { "filter": "vs:Kommune", "values": ["3032", "0301", "3805"] } },
-        { "code": "Kjonn", "selection": { "filter": "item", "values": ["2", "1"] } },
-        { "code": "Alder", "selection": { "filter": "item", "values": ["00"] } },
-        { "code": "ContentsCode", "selection": { "filter": "item", "values": ["Sykefraversprosent"] } },
-        { "code": "Tid", "selection": { "filter": "item", "values": [
-          "2020K1", "2020K2", "2020K3", "2020K4", "2021K1", "2021K2", "2021K3", "2021K4",
-          "2022K1", "2022K2", "2022K3", "2022K4", "2023K1", "2023K2", "2023K3", "2023K4",
-          "2024K1", "2024K2", "2024K3", "2024K4"
-        ] } }
-      ],
-      "response": { "format": "json-stat2" }
-    };
+    const url_Helse = "https://data.ssb.no/api/v0/no/table/12451/";
+
+const payload = {
+  query: [
+    {
+      code: "Region",
+      selection: {
+        filter: "agg_single:KommGjeldende",
+        values: ["3203", "0301", "3301", "3303", "4601"]
+      }
+    },
+    {
+      code: "Kjonn",
+      selection: {
+        filter: "item",
+        values: ["2", "1"]
+      }
+    },
+    {
+      code: "ContentsCode",
+      selection: {
+        filter: "item",
+        values: ["Sykefraversprosent"]
+      }
+    },
+    {
+      code: "Tid",
+      selection: {
+        filter: "item",
+        values: [
+          "2024K1",
+          "2024K2",
+          "2024K3",
+          "2024K4"
+        ]
+      }
+    }
+  ],
+  response: { format: "json-stat2" }
+};
+
+      
   
     try {
       const response = await fetch(url_Helse, {
